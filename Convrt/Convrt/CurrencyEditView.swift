84cfb2cc3e341bed795b59f29eef4ab604d96b2f
Hamon Riazy <hamon.riazy@gmail.com>
2015-06-21 23:15:30 +0100

    Textfield numberpad handling

--- Convrt/Convrt/CurrencyEditView.swift
+++ Convrt/Convrt/CurrencyEditView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CurrencyEditView: UIView {
+class CurrencyEditView: UIView, UITextFieldDelegate {
     
     @IBOutlet var codeLabel: UILabel?
     @IBOutlet var titleLabel: UILabel?
@@ -25,9 +25,42 @@ class CurrencyEditView: UIView {
     }
     
     func dismissView() {
+        self.amountTextField?.resignFirstResponder()
         UIView.animateWithDuration(0.5) { () -> Void in
             self.alpha = 0.0
         }
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldString = textField.text!
+        let newString = (oldString as NSString).stringByReplacingCharactersInRange(range, withString: string)
+        
+        if newString.characters.count == 0 {
+            return true
+        } else if newString.characters.count == 1 {
+            let decimalSeparator = NSLocale.currentLocale().objectForKey(NSLocaleDecimalSeparator) as! String
+            
+            if newString == decimalSeparator {
+                textField.text = "0."
+                return false
+            }
+        }
+        
+        
+        var regex: NSRegularExpression?
+        
+        do {
+            regex = try NSRegularExpression(pattern: "^[0-9]+(\\.[0-9]{0,2})?$", options: NSRegularExpressionOptions.CaseInsensitive)
+        } catch {
+            return false
+        }
+        
+        let numberOfMatches = regex!.numberOfMatchesInString(newString, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, newString.characters.count))
+        
+        return numberOfMatches > 0
+        
     }
 
     /*
